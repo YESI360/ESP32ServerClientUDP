dfcc071c1b1a2cc41b281a4de8c84e260d06eb4d
Yesi360VE <41321821+YESI360@users.noreply.github.com>
2024-01-15 18:11:51 -0300

    Update ChestESP32Reset2.ino
    comentarios para Inti

--- Version2/ChestESP32Reset2/ChestESP32Reset2.ino
+++ Version2/ChestESP32Reset2/ChestESP32Reset2.ino
@@ -12,20 +12,16 @@
     Alexandre Castonguay acastonguay@artengine.ca For Yesica Duarte. Thank you to EWMA library creator Arsen Torbarina.
 */
 #include <Plaquette.h>
-#include <Thresholder.h>
-
-// The analog input.35 = R1 ///////////////////////33NADA/////
-//AnalogIn in(32); // 32 = R3 1K
+#include <Thresholder.h>//dentro de la LIBRERIA PLAQUETTE
 
 AnalogIn in(34); // 34 = R2 2K
+int datoL2 = 0; // Indicates state of breathing "1" or "2"
+/////el 2 indica INHALACION [se estira el cinturon]. el 1 EXHALACION [cinturon vuelve al valor de reposo]
 
-// The serial output.
+//LIBRERIA PLAQUETTE
 StreamOut serialOut;
-
 AdaptiveNormalizer norm;
-
 Thresholder peakDetector(0.7, THRESHOLD_RISING, 0.5);
-//Thresholder peakDetector(0.7, THRESHOLD_FALLING, 0.5);
 
 ////////////// TFT SCREEN ///////////////////////////////////////
 #include <SPI.h>
@@ -54,13 +50,11 @@ unsigned int localPort = 8889;      // local port to listen on
 char packetBuffer[20 + 1]; //buffer to hold incoming packet---------------------
 ///// Fin Receiving UDP ////////////
 
-int datoL2 = 0; // Indicates state of breathing "1" or "2"
-
 unsigned long initRutina;
-unsigned long leTemps = 0;
-unsigned long elViejoTiempo = 0;
-unsigned long intervalleEntreResp = 0;
-String calMessage = "CC, ";//CAL CHEST
+unsigned long leTemps = 0;//el tiempo
+unsigned long elViejoTiempo = 0;//tiempo anterior
+unsigned long intervalleEntreResp = 0;//intervalo entre respiraciones
+String calMessage = "CC, ";//VALOR CALIBRADO CHEST
 String calibracion = "hello";
 
 bool invertScreen = false;
@@ -89,8 +83,8 @@ void begin() {
   ////// For Receiving UDP /////
   Serial.printf("UDP server on port %d\n", localPort);
   Udp.begin(localPort);
-  //////////////////////////////
-
+    
+  ///////////prender led para avisar que prendio ///////////////////
   pinMode (LED_BUILTIN, OUTPUT); // LED del ESP8266
 
   for ( int i = 0 ; i < 5 ; i++ ) { //el led parpadea 5 veces para saludar!
@@ -116,14 +110,13 @@ void begin() {
     ////////////////////////////////
   }
 
-  digitalWrite (LED_BUILTIN, HIGH);//apago led por las dudas reverse para los ESP8266
+  digitalWrite (LED_BUILTIN, HIGH);//apago led por las dudas 
 
   // Smooth over a window of 100ms (ie. one tenth of a second).
   // NOTE: Try changing the smoothing value to see how it affects the outputs.
   in.smooth(0.2);
-
   norm.time(10);
-
+////////////////////TIEMPO EN MILIS QUE TOMA PARA HACER LA CALIBRACION//////////////////
   // initRutina = millis() + 10000;///////////////////////////////////10mil 10seg
   //initRutina = millis() + 40000; //calibracion en CITY v.default.
   initRutina = millis() + 20000; //calCITY (version intrucciones 20) [reset en 15]
@@ -141,13 +134,12 @@ void step() {
   Serial.print(6 * in);
   Serial.println(" ");
 
-
   //Udp.beginPacket(ip, 8888);
   //String msg = String(localData.temp);
 
-  if (millis() < initRutina) //////////////////////solo envia msg calibracion
+  if (millis() < initRutina) //////////////////////solo envia valores para la calibracion. CONTINUOS
   {
-    String calibracion = String (norm * 6);
+    String calibracion = String (norm * 6);//*6 es para tener numeros mas grandes...
     String sendCal = calMessage + calibracion;
     Udp.beginPacket(ip, 8888);
     Udp.print(sendCal);
@@ -155,28 +147,24 @@ void step() {
 
     /////// Display values on the screen
     display.clearDisplay();
-
     display.setTextSize(1);             // Normal 1:1 pixel scale
     display.setTextColor(WHITE);        // Draw white text
     display.setCursor(0, 0);
     display.println(myIP);// Start at top-left corner
     //display.println(F("Client connected ?"));
-
     display.setTextSize(2);             // Draw 2X-scale text
     display.setTextColor(WHITE);
     display.setCursor(5, 16);
     display.println(F("CHEST"));
-
     display.setTextSize(4);             // Draw 2X-scale text
     display.setTextColor(WHITE);
     display.setCursor(6, 35);
     display.println(norm * 6 );
-
     display.display();
     delay(50);
   }
 
-  if (millis() > initRutina)////////////////////////envia msg sensado
+  if (millis() > initRutina)/////////////////envia valores ya calibrados. CAMBIOS DE ESTADO 1 Y 2
   {
 
     if (peakDetector == 1)
@@ -197,8 +185,7 @@ void step() {
       elViejoTiempo = leTemps;
 
     }
-    else if ( (millis() - elViejoTiempo) < intervalleEntreResp / 4) {
-      // si nous sommes à l'intérieur du tiers de l'ijntervalle de respiration
+    else if ( (millis() - elViejoTiempo) < intervalleEntreResp / 4) {// si estamos dentro de un tercio del intervalo de respiración. el 3 es un numero que daba mejor...
       digitalWrite (LED_BUILTIN, LOW);
       Udp.beginPacket(ip, 8888);
       Udp.print("chest, 2");
@@ -207,29 +194,23 @@ void step() {
 
       /////// Display values on the screen
       display.clearDisplay();
-
       if (invertScreen == true)
       {
         display.invertDisplay(true);
       }
-
       display.setTextSize(1);             // Normal 1:1 pixel scale
       display.setTextColor(WHITE);        // Draw white text
       display.setCursor(0, 0);
       display.println(myIP);// Start at top-left corner
       //display.println(F("Client connected ?"));
-
       display.setTextSize(2);             // Draw 2X-scale text
       display.setCursor(5, 16);
       display.setTextColor(WHITE);
       display.println(F("CHEST"));
-
       display.setTextSize(4);
       display.setCursor(6, 35);
       display.println(2);
-
       display.display();
-
     }
     else
     {
@@ -239,30 +220,25 @@ void step() {
       Udp.print("chest, 1");
       Udp.endPacket();
       Serial.println("CHEST, 1");
-
+        
       /////// Display values on the screen
       display.clearDisplay();
-
       if (invertScreen == true)
       {
         display.invertDisplay(true);
       }
-
       display.setTextSize(1);             // Normal 1:1 pixel scale
       display.setTextColor(WHITE);        // Draw white text
       display.setCursor(0, 0);
       display.println(myIP);// Start at top-left corner
       //display.println(F("Client connected ?"));
-
       display.setTextSize(2);             // Draw 2X-scale text
       display.setCursor(5, 16);
       display.setTextColor(WHITE);
       display.println(F("CHEST"));
-
       display.setTextSize(4);
       display.setCursor(6, 35);
       display.println(1);
-
       display.display();
     }
 
@@ -282,6 +258,7 @@ void step() {
       packetBuffer[n] = 0;
       //Serial.println("Contents:");
       Serial.println(packetBuffer);
+        //////////////////////recibe el dato de que toco el boton en el circuito del SERVER para "resetear"
       if (packetBuffer[0] == 'c') { // This compares the received message (we're sending '1')
         //"Resetting";
         invertScreen == true;
@@ -289,11 +266,4 @@ void step() {
         //ESP.restart();
       }
     }
-
-
-
-
-  }
-
-  //delay(200);///NO VA
 } // fin step
